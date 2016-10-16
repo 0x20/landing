c6e9e33a05ab302b1e6fde5f9744997d781db5c7
Bloemist <pieterdeplukker@hotmail.com>
2016-10-16 16:26:19 -0400

    testing

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
     <link href="css/ytplayer.css" rel="stylesheet" type="text/css" media="all" />
 		<link href="css/custom.css" rel="stylesheet" type="text/css" media="all" />
 		<link href='https://fonts.googleapis.com/css?family=Lato:300,400%7CRaleway:100,400,300,500,600,700%7COpen+Sans:400,500,600' rel='stylesheet' type='text/css'>
-		<!--<meta http-equiv="Refresh" content="300">-->
+		<meta http-equiv="Refresh" content="300">
 		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
 	</head>
 
