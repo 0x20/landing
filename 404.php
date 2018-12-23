3d92c1d8815f61b0051807640be8461360c00ee8
Bloemist <bloemistwashere@kwitz.net>
2018-12-23 06:19:35 +0100

    added 404 page

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php include 'layout_top.php'; ?>
+    <section class="cover fullscreen image-bg overlay parallax">
+        <div class="background-image-holder">
+            <img alt="image" class="background-image" src="img/boom.jpg">
+        </div>
+        <div class="container v-align-transform">
+            <div class="row">
+                <div class="col-sm-12 text-center">
+                  <h1 class="uppercase mb0 bold" style="font-size:350px;color: #e5692c;line-height:400px;">404</h1><br>
+		                    <h1 class="uppercase mb0">WHOOOOOOPS</h1><br><br><br>
+                        <a class="uppercase btn btn-lg mb32 mt-xs-40" href="index.php">Take me to a safe place!</a>
+                </div>
+            </div>
+            
+        </div>
+    </section>
+<?php include 'layout_bottom.php'; ?>
